b55a887c6cea05c77063f98788783791e3a2bb17
Joe Pea <joe@trusktr.io>
2023-09-21 14:06:22 -0700

    add a lume scene to the person page

--- src/routes/person/[personId]/PersonInfo.tsx
+++ src/routes/person/[personId]/PersonInfo.tsx
@@ -1,7 +1,17 @@
-import { Show } from "solid-js";
+"use client"
+
+import { Show, createEffect, createMemo, createSignal, onMount } from "solid-js"
 import { formatDate } from "~/utils/format";
 import { ExternalLinks } from "../../../components/ExternalLinks";
 import styles from "./PersonInfo.module.scss";
+
+let Motor, defineElements
+
+// Import lume on the client-side only.
+if (globalThis.window) {
+  ;({ defineElements, Motor } = await import("lume"))
+  defineElements()
+}
 
 function formatContent(content: string) {
   return content
@@ -20,6 +30,7 @@ function calculateAge(birthday: string, deathday?: string) {
 
 export function PersonInfo(props) {
   const profilePath = () => props.person.profile_path;
+  const profilePic = () => "https://image.tmdb.org/t/p/w370_and_h556_bestv2" + profilePath()
 
   const links = () => {
     const externalIds = props.person.external_ids;
@@ -32,19 +43,42 @@ export function PersonInfo(props) {
       : externalIds;
   };
 
+  const [rotationX, setRotationX] = createSignal(0)
+
+  onMount(() => {
+    // On mount, let's animation the box rotation.
+    Motor.addRenderTask(() => {
+      setRotationX((x) => ++x)
+    })
+  })
+
   return (
     <div class={`spacing ${styles.info}`}>
       <div class={styles.left}>
         <div class={styles.poster}>
           <Show when={profilePath()}>
             <img
-              src={
-                "https://image.tmdb.org/t/p/w370_and_h556_bestv2" +
-                profilePath()
-              }
+              src={profilePic()}
               alt={props.person.name}
             />
           </Show>
+        </div>
+
+        <div style="width: 100%; height: 200px; background: skyblue">
+          <lume-scene webgl>
+            <lume-point-light position="100 0 100" intensity={0.65}></lume-point-light>
+            <lume-ambient-light intensity={0.5}></lume-ambient-light>
+            <lume-camera-rig initial-distance={200} min-distance={100}></lume-camera-rig>
+            <lume-box
+              size="20 20 20"
+
+              //texture={profilePic()}
+              // Note, texture not working. Hmmmmm.
+
+              mount-point="0.5 0.5 0.5"
+              rotation={[rotationX()]}
+            ></lume-box>
+          </lume-scene>
         </div>
       </div>
 
